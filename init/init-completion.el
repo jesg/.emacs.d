4988a3223277f414a09a049fe5317be497859bab
jesg <gowanjason@gmail.com>
2014-11-18 04:30:35 -0600

    add C-n & C-p to navigate company selections

--- init/init-completion.el
+++ init/init-completion.el
@@ -7,6 +7,8 @@
      (define-key company-active-map (kbd "<tab>") nil)
      (define-key company-active-map (kbd "ESC") 'company-abort)
      (define-key company-active-map (kbd "<C-return>") 'company-abort)
+     (define-key company-active-map (kbd "C-n") 'company-select-next)
+     (define-key company-active-map (kbd "C-p") 'company-select-previous)
 
 
      (setq company-idle-delay 0)
